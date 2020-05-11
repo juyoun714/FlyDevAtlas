70e0f621d4e51c41ce1343ba0d106cc9e1974902
juyoun714 <juyoun714@gmail.com>
2020-05-11 13:41:47 -0700

    Update source.R

--- source.R
+++ source.R
@@ -10,16 +10,17 @@ library(dplyr)
 #dataA.light <- readRDS("data/dataA.light.rds")
 #exprAB.1 <- readRDS("data/exprAB.1.rds")
 
-#tmp0<- FetchData(dataA.light, vars = c("set","rep","time","sample","class1","type1","n","gene","expr",
-#                                       "tSNE_1", "tSNE_2"))
+#tmp0<- FetchData(dataA.light, vars = c("set","rep","time","sample","class1","type1","tSNE_1", "tSNE_2"))
+
 #dataA.light.tsne <- tmp0
 #saveRDS(dataA.light.tsne, file = "data/dataA.light.tsne.rds")
 
 # ====================================================== 
 # Data loading
 # ======================================================
-#dataTSNE <- readRDS(file = "data/dataA.light.rds")
+dataTSNE <- readRDS(file = "data/dataTSNE.rds")
 dataExpr <- readRDS(file = "data/dataExpr.rds")
+cordTSNE <- readRDS(file = "data/cordTSNE.rds")
 
 
 # =======================================================
