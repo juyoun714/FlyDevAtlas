a845c14c5259209d94d3632dbc0946e891b4b71e
juyoun714 <juyoun714@gmail.com>
2020-05-11 13:39:40 -0700

    Update app.R

--- app.R
+++ app.R
@@ -9,28 +9,51 @@ source("source.R")
 # ========================================================
 ui <- fluidPage(
      titlePanel("Drosohila Developmental Single-cell Atlas, 2020 (beta ver.)"),
-                #img(src="xx.png", height=. width = "100%")),
+     #img(src="xx.png", height=. width = "100%")),
      
-     sidebarLayout(
-          # inputs
-          sidebarPanel(
-               textInput("cell",label = "Cell type"),
-               textInput("gene",label = "Gene"),
-
-               helpText("tSNE: gene expression in clusters",
-                        "Heatmap: compare gene expression in all neurons",
-                        "Lineplot: gene expression trajectory within a cell type"),
-               
-               submitButton("Plot")
+     tabsetPanel(
+          tabPanel("tSNE", fluid = TRUE,
+                   sidebarLayout(
+                        sidebarPanel(width = 2,
+                                     textInput("gene",label = "Gene"),
+                                     helpText("tSNE: gene expression in clusters"),
+                                     submitButton("Plot")
+                                   ),
+                        mainPanel(fluidRow(
+                                  column(12, plotOutput("tsne")))
+                        )
+                   )
           ),
           
-               tabsetPanel(
-                    tabPanel("tSNE", plotOutput("tsne")), 
-                    tabPanel("Heatmap", plotOutput("heatmap", height = "100px")), 
-                    tabPanel("Line", plotOutput("lineplot"))
+          tabPanel("Heatmap", fluid = TRUE,
+                   sidebarLayout(
+                        sidebarPanel(width = 2,
+                                     textInput("gene",label = "Gene"),
+                                     helpText("Heatmap: compare gene expression in all neurons"),
+                                     submitButton("Plot")
+                                    ),
+                        mainPanel(fluidRow(
+                                   column(12, plotOutput("heatmap"), height = "250px"))
+                         )
                )
+          ),
+          
+          
+          tabPanel("Lineplot", fluid = TRUE,
+                   sidebarLayout(
+                        sidebarPanel(width = 2,
+                                     textInput("cell",label = "Cell type"),
+                                     textInput("gene",label = "Gene"),
+                                     helpText("Lineplot: gene expression trajectory within a cell type"),
+                                     submitButton("Plot")
+                        ),
+                         mainPanel(fluidRow(
+                              column(6, plotOutput("lineplot")))
+                         )
+                   )
           )
      )
+)
 
 
 
@@ -39,27 +62,37 @@ ui <- fluidPage(
 # Define server logic
 # ========================================================
 server <- function(input, output, session) {
-#     genes <- unlist(strsplit(input$gene, ","))
      
      output$tsne <- renderPlot({
-          FeaturePlot(dataTSNE, features = input$gene, label = T)+
+          ggplot(cordTSNE, aes(x=tSNE_1, y=tSNE_2)) +
+               geom_point(aes(x=tSNE_1, y=tSNE_2), colour = 'lightgrey', alpha = 0.5)+
+               geom_point(data = dataTSNE %>% filter(gene == input$gene), 
+                          aes(x=tSNE_1, y=tSNE_2, fill=expr), inherit.aes = FALSE) +
+               scale_fill_continuous(low = "#132B43",
+                                     high = "#56B1F7",
+                                     space = "Lab",
+                                     na.value = "grey50",
+                                     guide = "colourbar",
+                                     aesthetics = "fill") +
                theme_void()
           
      })
-         
+     
      output$heatmap <- renderPlot({
-          ggplot(dataExpr %>% filter(gene == input$gene), aes(fill=expr,x=type1,y=gene)) +
-               geom_tile(aes(fill = expr), colour='grey50')+ 
-               theme(axis.text.x = element_text(angle=90), hjust=1)
+          ggplot(dataExpr %>% filter(gene == input$gene), aes(fill=expr,x=type1, y=gene)) +
+               geom_tile(colour='grey50') +
+               scale_fill_gradient(low="white",high="darkblue", na.value = 'white')+
+               theme(axis.text.x = element_text(angle=90, hjust=1))
      })
      
      output$lineplot <- renderPlot({
-          ggplot(dataExpr %>% filter(type1 == input$cell), aes(x = time, y=expr,fill=gene,group=gene))+
+          ggplot(dataExpr %>% filter(type1 == input$cell) %>% filter(gene == input$gene), 
+                 aes(x = time, y=expr, fill=gene, group=gene))+
                geom_point() +
                geom_line() +
                theme_minimal()
      })
-
+     
 }
 
 
